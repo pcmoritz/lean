1aea0851aa1079fc677455c4ea1f981d627a5d88
pcmoritz <pcmoritz@gmail.com>
2017-06-07 20:13:47 +0000

    expose declarations and expressions

--- src/shell/leanpy.cpp
+++ src/shell/leanpy.cpp
@@ -9,6 +9,7 @@ namespace py = pybind11;
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <signal.h>
 #include <cstdlib>
 #include <getopt.h>
@@ -64,12 +65,12 @@ class LeanContext {
     unsigned trust_lvl = LEAN_BELIEVER_TRUST_LEVEL+1;
     environment env = mk_environment(trust_lvl);
     options opts;
-    io_state ios(opts, lean::mk_pretty_formatter_factory());
+    ios_ = io_state(opts, lean::mk_pretty_formatter_factory());
     tq_ = std::make_shared<st_task_queue>();
     set_task_queue(tq_.get());
     log_tree_ = std::make_shared<log_tree>();
     module_vfs_ = std::make_shared<fs_module_vfs>();
-    mod_mgr_ = std::make_shared<module_mgr>(module_vfs_.get(), log_tree_->get_root(), env, ios);
+    mod_mgr_ = std::make_shared<module_mgr>(module_vfs_.get(), log_tree_->get_root(), env, ios_);
     auto mod_info = mod_mgr_->get_module("/home/ubuntu/lean/library/standard.lean");
 
     // try {
@@ -79,9 +80,13 @@ class LeanContext {
     // }
     mod_infos_.push_back(mod_info);
     env_ = get_combined_environment(mod_mgr_->get_initial_env(), mod_infos_);
+    taskq().wait_for_finish(log_tree_->get_root().wait_for_finish());
   }
   declaration get(const name& name) {
     return env_.get(name);
+  }
+  void check(declaration const & d) {
+    lean::check(env_, d);
   }
 
   lean::initializer init_;
@@ -90,20 +95,21 @@ class LeanContext {
   std::shared_ptr<log_tree> log_tree_;
   std::shared_ptr<fs_module_vfs> module_vfs_;
   std::shared_ptr<module_mgr> mod_mgr_;
+  io_state ios_;
   buffer<std::shared_ptr<module_info const>> mod_infos_;
 };
 
-LeanContext initialize_lean() {
+std::shared_ptr<LeanContext> initialize_lean() {
   try {
-    return LeanContext();
+    return std::make_shared<LeanContext>();
   } catch (lean::throwable &ex) {
     std::cout << "exception has been thrown: " << ex.what() << std::endl;
   }
 }
 
-std::vector<name> get_module_declarations(const LeanContext& context) {
+std::vector<name> get_module_declarations(std::shared_ptr<LeanContext> context) {
   std::vector<name> result;
-  context.env_.for_each_declaration([&result](const declaration& d) {
+  context->env_.for_each_declaration([&result](const declaration& d) {
       result.push_back(d.get_name());
     });
   return result;
@@ -112,9 +118,21 @@ std::vector<name> get_module_declarations(const LeanContext& context) {
 PYBIND11_MODULE(leanpy, m) {
   m.doc() = "Python module for LEAN";
 
-  py::class_<LeanContext>(m, "LeanContext")
-    .def("get", &LeanContext::get);
-  py::class_<declaration>(m, "declaration");
+  py::class_<LeanContext, std::shared_ptr<LeanContext>>(m, "LeanContext")
+    .def("get", &LeanContext::get)
+    .def("check", &LeanContext::check);
+  py::class_<expr>(m, "expr")
+    .def("__repr__", [](const expr& e) {
+	std::stringstream ss;
+	ss << e;
+	return ss.str();
+      });
+  py::class_<declaration>(m, "declaration")
+    .def("__repr__", [](const declaration& d) {
+        return d.get_name().to_string(); // TODO(pcm): also print expression here
+      })
+    .def_property_readonly("value", &declaration::get_value);
+  py::class_<certified_declaration>(m, "certified_declaration");
   py::class_<environment>(m, "environment")
     .def("get", &environment::get);
   py::class_<name>(m, "name")
@@ -122,6 +140,17 @@ PYBIND11_MODULE(leanpy, m) {
 	return n.to_string();
       });
 
+  py::class_<level>(m, "level")
+    .def_property_readonly_static("zero", [](py::object) {
+	return mk_level_zero();
+      }, py::return_value_policy::reference)
+    .def_property_readonly_static("one", [](py::object) {
+	return mk_level_one();
+      }, py::return_value_policy::reference);
+
   m.def("initialize", &initialize_lean, "Initialize PyLean");
   m.def("get_module_declarations", &get_module_declarations);
+  m.def("mk_app", [](expr const & f, expr const & a) {
+      return mk_app(f, a, nulltag);
+    });
 }
